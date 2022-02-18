1315d0fa94fc33e1990460b0c477458f691572c0
Roman Sommer <roman@resonant-bytes.de>
2022-02-18 22:51:41 +0100

    jackdbus: import jack_autoconnect code

--- jack_dbus.py
+++ jack_dbus.py
@@ -2,6 +2,7 @@
 import dbus
 from functools import reduce
 import re
+import argparse
 
 # use dbus-send --session --print-reply --dest=org.jackaudio.service /org/jackaudio/... <interface> [Parameters]
 # -> tab completion is your friend (except for parameters)
@@ -198,3 +199,36 @@ def disconnect(s_port, d_port):
 
 def system_clients():
     return get_clients_by_name(r'system|firewire_pcm')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Autoconnect jack ports")
+    parser.add_argument('--sclient', default=".*")
+    parser.add_argument('--sport', default=".*")
+    parser.add_argument('--dclient', default=".*")
+    parser.add_argument('--dport', default=".*")
+    parser.add_argument('--number-of-ports', '-n', default=-1, type=int)
+    parser.add_argument('--sstart', default=0)
+    parser.add_argument('--dstart', default=0)
+
+    args = parser.parse_args()
+
+    shre = re.compile(args.sclient)
+    spre = re.compile(args.sport)
+    dhre = re.compile(args.dclient)
+    dpre = re.compile(args.dport)
+
+    ports = get_ports()
+    sources = [p for p in ports if shre.match(p.client) and spre.match(p.port)]
+    dests = [p for p in ports if dhre.match(p.client) and dpre.match(p.port)]
+
+    n = min(len(sources), len(dests))
+    if args.number_of_ports >= 0:
+        n = min(n, args.number_of_ports)
+    send = min(len(sources), args.sstart + n)
+    dend = min(len(dests), args.dstart + n)
+    pairs = zip(sources[args.sstart:send], dests[args.dstart:dend])
+    for s, d in pairs:
+        if s.is_output() and d.is_input():
+            print("{}\t-> {}".format(s,d))
+            connect(s, d)
