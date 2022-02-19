234ea5d79aee725ba61819d090f2ee83cd63ae63
Roman Sommer <roman@resonant-bytes.de>
2022-02-19 01:28:10 +0100

    add copyright and license header

--- jackdbus/__main__.py
+++ jackdbus/__main__.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+
+# Python library to manage JACK connections from python via DBUS and cli geek tool to create/break multiple connections at once using regular expressions.
+# Copyright (C) 2018-2022 Roman Sommer <roman@resonant-bytes.de>
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 import dbus
 from functools import reduce
 import re
